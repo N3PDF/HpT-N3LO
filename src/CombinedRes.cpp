608a66111e56a7e044d60af4af3454577e3a7887
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2021-06-10 11:32:23 +0200

    extract only NLO part

--- src/CombinedRes.cpp
+++ src/CombinedRes.cpp
@@ -69,40 +69,42 @@ std::complex<long double> CombinedRes::Matching(std::complex<long double> N,
 
 std::complex<long double> CombinedRes::CombinedResExpr(
     std::complex<long double> N, long double pt, int scheme) {
-  double pp = static_cast<double>(pt);
+  /* double pp = static_cast<double>(pt); */
   // take only real part. Does not work for complex
-  double nn = static_cast<double>(N.real());
+  /* double nn = static_cast<double>(N.real()); */
   std::complex<long double> mres;
-  std::vector<double> ResultsMellin;
-  std::vector<double> zero(2, 0.0);
+  /* std::vector<double> ResultsMellin; */
+  /* std::vector<double> zero(2, 0.0); */
 
   // Compute exact FO from HpT-MON
-  if (ORD == 0) {
-    ResultsMellin = zero;
-  } else {
-    ResultsMellin = MELLINPARTONIC->partonichiggsdpt(pp, nn);
-  }
-  std::vector<long double> ExactMellin(ResultsMellin.begin(),
-                                       ResultsMellin.end());
+  /* if (ORD == 0) { */
+  /*   ResultsMellin = zero; */
+  /* } else { */
+  /*   ResultsMellin = MELLINPARTONIC->partonichiggsdpt(pp, nn); */
+  /* } */
+  /* std::vector<long double> ExactMellin(ResultsMellin.begin(), */
+  /*                                      ResultsMellin.end()); */
 
   // Compute approximation from resummations
   if (scheme == 3) // High Energy
   {
-    std::complex<long double> ExactMellinCmpx(ExactMellin[0], 0.);
+    /* std::complex<long double> ExactMellinCmpx(ExactMellin[0], 0.); */
     std::complex<long double> HighEnergyMellin = HIGHENERGY->HighEnergyExpExpr(N, pt);
-    return ExactMellinCmpx + HighEnergyMellin;
+    /* return ExactMellinCmpx + HighEnergyMellin; */
+    return HighEnergyMellin;
   } else
   {
     std::complex<long double> SptMellin = SMALLPT->SmallptExpExpr(N, pt);
     /* std::complex<long double> ThresMellin = THRESHOLD->ThresExpExpr(N, pt); */
     std::complex<long double> xThresMellin = MELLIN->xSpaceThres(N, pt);
 
-    std::complex<long double> ExactMellinCmpx(ExactMellin[0], 0.);
+    /* std::complex<long double> ExactMellinCmpx(ExactMellin[0], 0.); */
     mres = (1. - Matching(N, pt, scheme)) * SptMellin +
           Matching(N, pt, scheme) * xThresMellin;
           /* Matching(N, pt, scheme) * ThresMellin; */
 
-    return ExactMellinCmpx + mres;
+    /* return ExactMellinCmpx + mres; */
+    return mres;
 
   }
 }

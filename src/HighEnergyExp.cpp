5be622ae5d019699f289db48ca1bb6c9dfe7e1da
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2021-06-16 16:30:49 +0200

    add x-space HE

--- src/HighEnergyExp.cpp
+++ src/HighEnergyExp.cpp
@@ -38,34 +38,9 @@ HighEnergyExp::HighEnergyExp(int order, int channel, void *params) {
   MUR2 = static_cast<long double>(std::pow(param.mur, 2));
   MUF2 = static_cast<long double>(std::pow(param.muf, 2));
 
-  LF = std::log(MH2 / MUF2);
-  LR = std::log(MH2 / MUR2);
-
-  aass = static_cast<long double>(
-      param.alphas);  // TODO; long double-check PI normalization
+  aass = static_cast<long double>(param.alphas);
   SROOT = static_cast<long double>(param.sroot);
   SIGMA0 = static_cast<long double>(param.sigma0);
-
-  // Compute Beta Functions
-  Beta0 = (11. * CA - 2. * NF) / (12. * M_PIl);
-  Beta1 = ((17. * CA * CA - 5. * CA * NF - 3. * CF * NF) * 2. / 3.) /
-          (16. * M_PIl * M_PIl);
-  Beta2 = ((2857. / 54. * CA * CA * CA +
-            (CF * CF - 205. / 18. * CF * CA - 1415. / 54. * CA * CA) * NF +
-            (11. / 9. * CF + 79. / 54. * CA) * NF * NF)) /
-          std::pow(4. * M_PIl, 3);
-
-  // One & too loop Cusp Anomalous Dimensions
-  // In the following, the 1/PI is contained in the definitions
-  // of the cusp Anomalous dimensions not in the normalization of as
-  Bth1g = -Beta0;
-  Ath1g = CA / M_PIl;
-  Ath1q = CF / M_PIl;
-  Bth1q = -3. / 4. * CF / M_PIl;
-  Ath2g = (CA / 2. * (CA * (67. / 18. - zeta2) - 5. / 9. * NF)) /
-          std::pow(M_PIl, 2);
-  Ath2q = (CF / 2. * (CA * (67. / 18. - zeta2) - 5. / 9. * NF)) /
-          std::pow(M_PIl, 2);
 }
 
 HighEnergyExp::~HighEnergyExp() {}
@@ -73,6 +48,21 @@ HighEnergyExp::~HighEnergyExp() {}
 //==========================================================================================//
 //                               gg-channel NLO pt-distributions //
 //------------------------------------------------------------------------------------------//
+
+std::complex<long double> HighEnergyExp::C1ggx(long double x,
+                                              long double xp) {
+  std::complex<long double> result;
+  result = 2 * CA / M_PIl / xp;
+  return result;
+}
+
+std::complex<long double> HighEnergyExp::C2ggx(long double x,
+                                              long double xp) {
+  std::complex<long double> result;
+  result = 4 * std::pow(CA, 2) / std::pow(M_PIl, 2) * (log(xp) / xp) *
+           std::log(x);
+  return result;
+}
 
 std::complex<long double> HighEnergyExp::C1gg(std::complex<long double> NN,
                                               long double xp) {
@@ -119,6 +109,45 @@ std::complex<long double> HighEnergyExp::HighEnergyExpExpr(
     {
       if ((CHANNEL == 0) || (CHANNEL == 5)) {
         result += std::pow(aass, 2) * C2gg(N, xp);
+      }
+      if ((CHANNEL == 1) || (CHANNEL == 5)) {
+        return zero;
+      }
+      if ((CHANNEL == 2) || (CHANNEL == 5)) {
+        return zero;
+      }
+    } break;
+  }
+
+  return 2. * pt / MH2 * SIGMA0 * result;
+}
+
+// TODO: change this into a vector
+std::complex<long double> HighEnergyExp::HighEnergyExpExprX(
+    long double x, long double pt) {
+  // TODO: re-check definition MH2 vs. Qs2
+  long double xp = std::pow(pt, 2) / MH2;
+  std::complex<long double> zero(0., 0.);
+  std::complex<long double> result(0., 0.);
+
+  switch (ORD) {
+      // TODO: Match CASES with HpT-MON
+    case (0):  // order as^1
+    {
+      if ((CHANNEL == 0) || (CHANNEL == 5)) {
+        result += aass * C1ggx(x, xp);
+      }
+      if ((CHANNEL == 1) || (CHANNEL == 5)) {
+        result += zero;
+      }
+      if ((CHANNEL == 2) || (CHANNEL == 5)) {
+        result += zero;
+      }
+    } break;
+    case (1):  // order as^2
+    {
+      if ((CHANNEL == 0) || (CHANNEL == 5)) {
+        result += std::pow(aass, 2) * C2ggx(x, xp);
       }
       if ((CHANNEL == 1) || (CHANNEL == 5)) {
         return zero;
